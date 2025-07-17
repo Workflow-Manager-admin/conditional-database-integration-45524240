56435b21005bc1112ef85277f73208df9e058da1
Kavia AI <production@kavia.ai>
2025-07-17 16:04:30 +0000

    Auto-commit before merging to kavia-main - conditional-database-integration-45524240

--- .init/.linter.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd /home/kavia/workspace/code-generation/conditional-database-integration-45524240/frontend_react_app
-npm run build
-EXIT_CODE=$?
-if [ $EXIT_CODE -ne 0 ]; then
-   exit 1
-fi
-
